d182038dd40db435825d4ed81f35562303247cf6
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-12-14 12:26:49 -0700

    POST a new action for projects by ID works

--- routers/actions.js
+++ routers/actions.js
@@ -4,13 +4,19 @@ const router = express.Router();
 
 // GET all actions
 router.get("/", (req, res) => {
-    db.get()
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-          console.log(err)
-          res.sendStatus(500)
-      });
-  });
-  module.exports = router;
+  db.get()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+// POST a new action for a project by ID
+router.post("/", (req, res) => {
+  db.insert(req.body)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ message: "Could not create action" }));
+});
+module.exports = router;
