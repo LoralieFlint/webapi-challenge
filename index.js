8319fb4e22ad8fe3835b4cbf4427b16de2f8fe7e
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-12-14 10:32:47 -0700

    got server up and running with nodemon

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const express = require("express");
+
+const app = express();
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8000;
+
+// app.use((req, res, next) => {
+// 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
+// 	next()
+// })
+
+// app.get("/", (req, res) => {
+// 	res.json({
+// 		message: "Welcome to our API",
+// 		cohort: process.env.LAMBDA_COHORT,
+// 		secret: process.env.SUPER_SECRET_API_KEY,
+// 	})
+// })
+
+app.listen(port, host, () => {
+  console.log(`Running at http://${host}:${port}`);
+});
